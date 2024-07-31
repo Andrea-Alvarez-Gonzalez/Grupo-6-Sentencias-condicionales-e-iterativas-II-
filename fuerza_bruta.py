cb74dd49fac74de5ea00753bfc47b3d4836b11ac
LiuvaSalas <liuvasalas01@gmail.com>
2024-07-30 21:34:55 -0400

    avance del ejercicio 3: agregue linea de codigo para almacenar la variable de intentos para luego sumar y guardar el numero de intentos +1 en cada ciclo

---
+++ fuerza_bruta.py
@@ -0,0 +1,12 @@
+#Librerias
+from getpass import getpass
+from string import ascii_lowercase
+
+#Definimos la base de los intentos
+intento = 0
+
+password = getpass("Ingresa tu contraseña: ")
+print(ascii_lowercase)
+
+
+
